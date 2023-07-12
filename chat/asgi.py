5226ba9f760cbd0111423c515a70b8ba1fd2b431
msinankk <sinanjfs@gmail.com>
2023-07-12 11:18:42 +0530

    [updt] channel layer

--- chat/asgi.py
+++ chat/asgi.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
+from channels.http import AsgiHandler
+from channels.routing import ProtocolTypeRouter
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http": AsgiHandler(),
+    }
+)
--- chat/settings.py
+++ chat/settings.py
@@ -123,3 +123,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+ASGI_APPLICATION = "config.asgi.application"
+
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("localhost", 8080)],
+        },
+    },
+}
