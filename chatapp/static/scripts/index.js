9f8e418d1504bc3efb3aa5b475b1bdb96addd463
msinankk <sinanjfs@gmail.com>
2023-07-12 16:14:59 +0530

    [updt] message appending and styles

--- chatapp/static/scripts/index.js
+++ chatapp/static/scripts/index.js
@@ -6,23 +6,28 @@ $(document).ready(function () {
   chatSocket.onmessage = function (e) {
     let data = JSON.parse(e.data);
     console.log("Data:", data);
-    
-    if (data.type === "chat") {
-      let messages = document.getElementById("chat-container");
 
-      messages.append(
-        `<div>
-          <p>${data.message}</p>
-          </div>`
-      );
+    if (data.type === "chat") {
+      var messagesContainer = $("#chat-container");
+
+      // Create a new <div> element
+      var newDiv = $("<div class='chat-message'>");
+
+      // Create a <p> element and set its content to 'data.message'
+      var newParagraph = $("<p>").text(data.message);
+
+      // Append the <p> element to the <div> element
+      newDiv.append(newParagraph);
+
+      // Append the <div> element to the 'messages' container
+      messagesContainer.append(newDiv);
     }
   };
 
-  $("#message-form").submit(function (e) { 
+  $("#message-form").submit(function (e) {
     e.preventDefault();
     chatMessage = $("#message").val();
-    chatSocket.send(JSON.stringify({"chat":chatMessage}))
-
+    chatSocket.send(JSON.stringify({ chat: chatMessage }));
   });
   // let form = document.getElementById("message-form");
   // form.addEventListener("submit", (e) => {
