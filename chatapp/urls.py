c65417bd4d1b5b41842bcbe9af83e34bc1e7d869
msinankk <sinanjfs@gmail.com>
2023-07-12 12:05:42 +0530

    [updt] url conf

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -5,7 +5,11 @@ This module is used to register the URL routes/path with Python methods
 """
 from django.urls import path
 from chatapp import views
+from chatapp import routings
+
 
 urlpatterns = [
     path("",views.chat)
 ]
+
+urlpatterns += routings.websocket_urlpatterns
