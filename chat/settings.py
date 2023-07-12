89a1d5ada836e9b7c661b7879a862ca6304b6483
msinankk <sinanjfs@gmail.com>
2023-07-12 11:48:02 +0530

    [updt] channel layer conf

--- chat/settings.py
+++ chat/settings.py
@@ -129,9 +129,6 @@ ASGI_APPLICATION = "config.asgi.application"
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("localhost", 8080)],
-        },
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
     },
 }
