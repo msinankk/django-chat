03c4fc2be5dc622f22bdc5256f55291ce53a9d2b
msinankk <sinanjfs@gmail.com>
2023-07-12 17:22:03 +0530

    [add] two way channel

--- chatapp/consumers/consumers.py
+++ chatapp/consumers/consumers.py
@@ -14,21 +14,29 @@ class ChatConsumer(WebsocketConsumer):
     """
 
     def connect(self):
+        self.room_group_name = "test"
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name,
+        )
         self.accept()
-        self.channel_layer = "test"
         self.send(json.dumps({"type": "success message"}))
 
     def receive(self, text_data=None, bytes_data=None):
         super().receive(text_data, bytes_data)
         text_data_json = json.loads(text_data)
         message = text_data_json["chat"]
-        self.send(
-            json.dumps(
-                {
-                    "type": "chat",
-                    "message": message,
-                }
-            )
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "message": message,
+            },
         )
 
+        return
+
+    def chat_message(self, event):
+        message = event["message"]
+        self.send(text_data=json.dumps({"type": "chat", "message": message}))
         return
