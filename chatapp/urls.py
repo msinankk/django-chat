71707c7ef857e580d3d353c9a55a38a7ee656ffb
msinankk <sinanjfs@gmail.com>
2023-07-12 10:20:24 +0530

    [init] first route

---
+++ chatapp/urls.py
@@ -0,0 +1,11 @@
+"""
+urls.py
+
+This module is used to register the URL routes/path with Python methods
+"""
+from django.urls import path
+from chatapp import views
+
+urlpatterns = [
+    path("",views.home)
+]
--- chatapp/views.py
+++ chatapp/views.py
@@ -1,3 +1,14 @@
+"""
+views.py
+
+This module is used to map the methods with the routes
+"""
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    """
+    Chat project home page method
+    """
+    return render(request,"index.html")
