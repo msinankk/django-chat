f92e12adb829f90e4287497b65e2827084884700
msinankk <sinanjfs@gmail.com>
2023-07-12 12:00:28 +0530

    [updt] consumer script and routing scripts

--- chatapp/consumers/consumers.py
+++ chatapp/consumers/consumers.py
@@ -3,3 +3,46 @@ consumer.py
 
 This module is used to handle websocket requests
 """
+
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    """
+    ChatConsumer
+    """
+
+    async def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = "chat_%s" % self.room_name
+
+        # Join room group
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+
+        await self.accept()
+
+    async def disconnect(self, _close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data["message"]
+        sender = data["sender"]
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {"type": "chat_message", "message": message, "sender": sender},
+        )
+
+    async def chat_message(self, event):
+        """
+        chat message method
+        """
+        message = event["message"]
+        sender = event["sender"]
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({"message": message, "sender": sender}))
--- chatapp/routings.py
+++ chatapp/routings.py
@@ -3,3 +3,11 @@ routings.py
 
 This module is used to handle websocket requests
 """
+
+from django.urls import re_path
+
+from chatapp.consumers.consumers import ChatConsumer
+
+websocket_urlpatterns = [
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
+]
