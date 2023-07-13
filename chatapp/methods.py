f193baa0b80ab452570d685657bc3fbcf74bee71
msinankk <sinanjfs@gmail.com>
2023-07-13 11:57:22 +0530

    [updt] socke connect method

--- chatapp/methods.py
+++ chatapp/methods.py
@@ -3,6 +3,7 @@ methods.py
 
 This module is used to write custom methods to chatapp
 """
+from urllib.parse import parse_qs
 from chatapp.models import ChatRoom
 
 
@@ -15,3 +16,17 @@ def private_room(user1, user2):
     room.users.add([user1, user2])
     room.save()
     return room
+
+
+def decode_query_string(query_string):
+    """
+    Query string decode method
+    """
+    # Parse the query string into a dictionary
+    decoded_data = parse_qs(query_string)
+
+    # Process the decoded data as needed
+    # ...
+
+    # Return the decoded data
+    return decoded_data
