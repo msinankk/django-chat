f07ceff4153de66d0500cdaa61e8b5346ef4c1af
msinankk <sinanjfs@gmail.com>
2023-07-13 12:13:28 +0530

    [updt] private room method

--- chatapp/consumers/consumers.py
+++ chatapp/consumers/consumers.py
@@ -7,7 +7,7 @@ import json
 from django.contrib.auth.models import User
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
-from chatapp.methods import decode_query_string
+from chatapp.methods import decode_query_string, private_room
 
 
 class ChatConsumer(WebsocketConsumer):
@@ -18,11 +18,15 @@ class ChatConsumer(WebsocketConsumer):
     def connect(self):
         session = self.scope["session"]
         data = self.scope["query_string"].decode("utf-8")
-        user_id = session["_auth_user_id"]
-        user = User.objects.get(id=user_id)
+        user1_id = session["_auth_user_id"]
         data = decode_query_string(data)
+        user2_id = data["user_id"]
 
-        self.room_group_name = user.chatroom_set.first().name
+        room = private_room(user1_id,user2_id)
+        print("----------------")
+        print(room)
+        print("----------------")
+        self.room_group_name = room.name
 
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
